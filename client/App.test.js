cbe312a4a185dba9eb0b01c2a1e6cfbdc232e85d
Edward Timmer <edtimmer@gmail.com>
2019-03-17 19:16:33 -0400

    modified readme

--- client/App.test.js
+++ client/App.test.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import Mantra from './components/Mantra';
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders without error', () => {
-  // const wrapper = shallow(<Mantra />)
+  const wrapper = shallow(<Mantra store={store} />)
+  // expect(wrapper).toBeTruthy();
+  console.log(wrapper.debug());
 });
 
